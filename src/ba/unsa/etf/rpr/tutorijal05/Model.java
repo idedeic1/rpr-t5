228e7fb20fc3e7f9d835619a91fef4cb27ef47e4
Ibrahim Dedeic <idedeic1@etf.unsa.ba>
2018-11-14 15:07:32 +0100

    druga poruka

--- src/ba/unsa/etf/rpr/tutorijal05/Model.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Model.java
@@ -1,9 +1,21 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
 public class Model {
-    public float Calculate(long number1, long number2, String operator){
+    public float calculate(long number1, long number2, String operator){
+        switch(operator){
+            case"+":
+                return number1+number2;
+            case"-":
+                return number1-number2;
+            case"x":
+                return number1*number2;
+            case"/":
+                if(number2 == 0) return 0;
+                return number1/number2;
+            default:
+                return 0;
+        }
 
 
-        return 0;
     }
 }
