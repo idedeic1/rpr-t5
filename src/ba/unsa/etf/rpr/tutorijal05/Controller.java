51a564e633981b19c6fb47d4f3ac82d2ce395497
Ibrahim Dedeic <idedeic1@etf.unsa.ba>
2018-11-14 17:32:58 +0100

    evo radi kobajagi

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,17 +1,28 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class Controller {
-        @FXML
+
         public Label result;
         private long number1 = 0;
         private String operator = " ";
         private boolean start = true;
         private Model model = new Model();
+        private SimpleStringProperty labela;
+
+        public String getLabela(){
+            return labela.get();
+        }
+
+        public SimpleStringProperty labelaProperty(){
+            return labela;
+        }
+
         @FXML
         public void processNumbers(ActionEvent event){
                 if(start){
@@ -27,11 +38,11 @@ public class Controller {
                 if(!value.equals("=")){
                     if(!operator.isEmpty()) return;
                     operator = value;
-                    number1 = Long.parseLong(result.getText());
+                    number1 = Long.parseLong(getLabela());
                     result.setText("");
                 } else{
                         if(operator.isEmpty()) return;
-                        long number2 = Long.parseLong(result.getText());
+                        long number2 = Long.parseLong(getLabela());
                         float output = model.calculate(number2, number2, value);
                         result.setText(String.valueOf(output));
                         operator = "";
